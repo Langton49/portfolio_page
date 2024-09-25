9c7e36e52748f0b1f978881088f777746166ac99
Munashe Mukweya <munashemukweya2022@gmail.com>
2024-09-25 16:53:16 -0500

    N/A

--- 3D_Interactive/mainPage.js
+++ 3D_Interactive/mainPage.js
@@ -32,7 +32,7 @@ function turnOn(content){
 async function createHTMLTexture() {
     const htmlContent = await loadHTMLContent();
     cssRenderer = new CSS3DRenderer();
-    cssRenderer.setSize(1920, 1080);
+    cssRenderer.setSize(2520, 1080);
     cssRenderer.domElement.style.position = 'absolute';
     cssRenderer.domElement.style.top = '0';
     cssRenderer.domElement.style.left = '0';
